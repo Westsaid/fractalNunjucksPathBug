80e65e5fa7715e9dff3e11c8f0b4a3517250557a
Said Adibilarijani <Said.Adibilarijani@syzygy.de>
2021-01-29 15:52:40 +0100

    Initialization

---
+++ server.js
@@ -0,0 +1,9 @@
+/* eslint-env node */
+const fractal = require('./fractal.config.js');
+const server = fractal.web.server();
+
+server.start().then(function(){
+  console.log(`Fractal server is now running.`);
+});
+
+server.stop();
